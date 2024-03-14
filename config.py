c9b8f5d98aed55339aaa2f9d9c773648e892d2b0
mehulbhuradia <mehulbhuradia007@gmail.com>
2024-03-14 16:07:41 +0530

    fixed config

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ def create_config():
 
     training = config.training = ml_collections.ConfigDict()
     training.training_iters = 50_000_000
-    training.checkpoint_freq = 50
-    training.eval_freq = 50
+    training.checkpoint_freq = 50_000
+    training.eval_freq = 50_000
     training.batch_size = 128
 
     training.ode_sampling = False
