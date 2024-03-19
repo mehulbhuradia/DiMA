6de2aca06846234d37386b9a7b9dbe5c14585953
mehulbhuradia <mehulbhuradia007@gmail.com>
2024-03-19 16:55:13 +0530

    name change

--- train_diffusion.py
+++ train_diffusion.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     config = create_config()
     timstamp = str(time.time()).replace(".","")
     config.training.checkpoints_folder = './checkpoints/'+ timstamp + '/'
-    config.checkpoints_prefix = "DiMA_CROSS"+timstamp+"_"+config.data.dataset
+    config.checkpoints_prefix = "DiMA_CROSS"+timstamp+"_"+config.data.dataset+"_"+config.model.hidden_size
     config.training.batch_size_per_gpu = config.training.batch_size 
     config.device = "cuda" if torch.cuda.is_available() else "cpu"
     config.project_name = 'proteins'
